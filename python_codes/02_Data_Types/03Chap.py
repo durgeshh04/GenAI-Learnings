ec956e99daacd0d3294b17451b19d46f41dfb9d3
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-28 02:30:13 +0530

    chap 3 completed

--- python_codes/02_Data_Types/03Chap.py
+++ python_codes/02_Data_Types/03Chap.py
@@ -1,40 +1,67 @@
+# We import third party packages using import in python
+import math
+
+class Operations:
 # def is a syntax for defining function in python
+    def takeInputs(self, n):
+        if n != 6:
+            a = float(input("Enter first no: "))
+            b = float(input("Enter second no: "))
+            return a, b
+        else:
+            a = float(input("Enter value to check the value type (even or odd): "))
+            return a
 
-def addition(a, b):
-    print(a + b)
+    def addition(self, a, b):
+        print(f"Addition: {a + b}")
 
-def substraction(a, b):
-    print(a - b);
+    def substraction(self, a, b):
+        print(f"Substraction: {a - b}");
 
-def multiplication(a, b):
-    print(a * b)
+    def multiplication(self, a, b):
+        print(f"Multiplication: {a * b}")
 
-def division(a, b):
-    print(a / b)
+    def division(self, a, b):
+        res = a / b
+        print(f"Division: {res}")
+        print(f"Ceil Division: {math.ceil(res)}")
 
-def floorDivision(a, b):
-    print(a // b)
+    def floorDivision(self, a, b):
+        print(f"Floor Division: {a // b}")
+    
+    def findEvenOrOdd(self, a):
+        if a % 2 == 0:
+            print(f"{a} is even value")
+        else:
+            print(f"{a} is odd value")
+
 
 # input() function is used in python for taking user inputs
+n = int(input("Enter any value in range of 1 to 7 to perform operation: "))
 
-n = int(input("Enter any value in range of 1 to 5 to perform operation: "))
-a = 10
-b = 5.5
 
+obj = Operations()
 
 # match is like a switch in other programming languages
-
 match n:
     case 1: 
-        addition(a , b)
+        a, b = obj.takeInputs(n)
+        obj.addition(a , b)
     case 2: 
-        substraction(a, b)
+        a, b = obj.takeInputs(n)
+        obj.substraction(a, b)
     case 3: 
-        multiplication(a, b)
+        a, b = obj.takeInputs(n)
+        obj.multiplication(a, b)
     case 4: 
-        division(a, b)
+        a, b = obj.takeInputs(n)
+        obj.division(a, b)
     case 5: 
-        floorDivision(a, b)
+        a, b = obj.takeInputs(n)
+        obj.floorDivision(a, b)
+    case 6:
+        a, b = obj.takeInputs(n)
+        obj.findEvenOrOdd(a)
     case _:
         print("Invalid case")
 
