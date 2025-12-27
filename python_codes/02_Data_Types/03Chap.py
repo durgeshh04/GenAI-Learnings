77a1c2d9a335effc3411e20e2c8356142aa79570
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-27 23:58:26 +0530

    comments added

--- python_codes/02_Data_Types/03Chap.py
+++ python_codes/02_Data_Types/03Chap.py
@@ -1,3 +1,5 @@
+# def is a syntax for defining function in python
+
 def addition(a, b):
     print(a + b)
 
@@ -13,9 +15,14 @@ def division(a, b):
 def floorDivision(a, b):
     print(a // b)
 
+# input() function is used in python for taking user inputs
+
 n = int(input("Enter any value in range of 1 to 5 to perform operation: "))
 a = 10
 b = 5.5
+
+
+# match is like a switch in other programming languages
 
 match n:
     case 1: 
