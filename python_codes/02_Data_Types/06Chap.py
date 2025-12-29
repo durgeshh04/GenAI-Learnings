eb359877e81e71c39080fcc8036acdba63d3232c
durgeshh04 <durgeshchaudhari199@gmail.com>
2025-12-29 23:47:25 +0530

    code fixed

--- python_codes/02_Data_Types/06Chap.py
+++ python_codes/02_Data_Types/06Chap.py
@@ -1,5 +1,10 @@
-# This section is all about Tuples
+# This section is all about Tuple
 
 names = ("Tiger", "Eagle", "Wolf", "Honey Bagger")
 
 print(f"My tuple: {names}")
+
+(name1, name2, name3, name4) = names    # If you don't extract tuples properly to the length then it will throw error: (name1, name2, name3) = names
+                                        # ValueError: too many values to unpack (expected 3)
+
+print(f"{name1}, {name2}, {name3}, {name4}")
