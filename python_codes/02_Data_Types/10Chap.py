ed0b577ee5087b5bc3e1564ee5241a5b9d2129a5
durgeshh04 <durgeshchaudhari199@gmail.com>
2026-01-06 23:59:36 +0530

    methods added of the dict

--- python_codes/02_Data_Types/10Chap.py
+++ python_codes/02_Data_Types/10Chap.py
@@ -14,3 +14,11 @@ order2["price"] = 13000
 print(f"New order: {order2}")
 del order2["price"]
 print(f"New order: {order2}")
+
+print(f"price is available for order 1? {"price" in order}")
+
+# methods to fetch the dictionary values, keys and items:
+
+print(f"keys of dict: {order.keys()}")
+print(f"value of the dict: {order.values()}")
+print(f"items of the dict: {order.items()}")
