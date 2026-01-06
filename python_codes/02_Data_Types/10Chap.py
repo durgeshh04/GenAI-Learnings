ee3d4acffcf8906db5cd5ebe4103509a81bec338
durgeshh04 <durgeshchaudhari199@gmail.com>
2026-01-06 23:49:00 +0530

    new dictionary creation method

--- python_codes/02_Data_Types/10Chap.py
+++ python_codes/02_Data_Types/10Chap.py
@@ -1,3 +1,14 @@
 order = dict(category = "Clothes", type = "Tshirt", size = "L", price = 450)
 
 print(f"Your order: {order}")
+
+# accessing a value of dict
+print(f"Category of your order: {order["category"]}")
+
+# creating dict with new way
+
+order2 = {}
+order2['category'] = "Electronics"
+order2['type'] = "TV"
+
+print(f"New order: {order2}")
