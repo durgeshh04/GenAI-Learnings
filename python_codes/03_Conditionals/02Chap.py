a74cbc0bfd0821409618e7392d8ac3ac142077a4
durgeshh04 <durgeshchaudhari199@gmail.com>
2026-01-09 23:51:02 +0530

    exercise solved for the condistionals

---
+++ python_codes/03_Conditionals/02Chap.py
@@ -0,0 +1,6 @@
+snacks = input("Enter your order here: ").lower()
+
+if snacks == "samosa" or snacks == "cookies":
+    print("You're order is confirmed")
+else:
+    print("Sorry the item is unavailable")
