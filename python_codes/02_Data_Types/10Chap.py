11213d2ac53a23b883e0911bca651c2c0362a858
durgeshh04 <durgeshchaudhari199@gmail.com>
2026-01-09 23:21:20 +0530

    exercise completed on dictionary

--- python_codes/02_Data_Types/10Chap.py
+++ python_codes/02_Data_Types/10Chap.py
@@ -28,3 +28,22 @@ print(f"items of the dict: {order.items()}")
 
 for key, val in order.items():
     print(f"{key}: {val}")
+
+
+# Exercise on dictionary:
+phonebook = {  
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}  
+# your code goes here
+
+phonebook["Jake"] = 938273443
+del phonebook["Jill"]
+
+# testing code
+if "Jake" in phonebook:  
+    print("Jake is listed in the phonebook.")
+    
+if "Jill" not in phonebook:      
+    print("Jill is not listed in the phonebook.")
